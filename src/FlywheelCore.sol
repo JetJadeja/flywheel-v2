81bed4b953f32095cd7326c8154c2eb0997a989c
JetDeveloping <jetjadeja@gmail.com>
2022-03-15 12:16:56 -0700

    Add FlywheelCore documentation

--- src/FlywheelCore.sol
+++ src/FlywheelCore.sol
@@ -8,173 +8,269 @@ import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {IFlywheelRewards} from "./interfaces/IFlywheelRewards.sol";
 import {IFlywheelBooster} from "./interfaces/IFlywheelBooster.sol";
 
-/**
- @title Flywheel Core Incentives Manager
- @notice Flywheel is a general framework for managing token incentives.
-         It is comprised of the Core (this contract), Rewards module, and optional Booster module.
-
-         Core is responsible for maintaining reward accrual through reward indexes. 
-         It delegates the actual accrual logic to the Rewards Module.
-
-         For maximum accuracy and to avoid exploits, rewards accrual should be notified atomically through the accrue hook. 
-         Accrue should be called any time tokens are transferred, minted, or burned.
- */
+/// @title Flywheel Core Incentives Manager
+/// @author The Tribe
+/// @notice Responsible for maintaining reward accrual through reward indexes.
+/// It delegates the actual reward calculation accrual logic to the FlywheelRewards module.
 contract FlywheelCore is Auth {
     using SafeTransferLib for ERC20;
 
-    event AddMarket(address indexed newMarket);
+    /*///////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
 
-    event FlywheelRewardsUpdate(address indexed oldFlywheelRewards, address indexed newFlywheelRewards);
-
-    event AccrueRewards(ERC20 indexed cToken, address indexed owner, uint rewardsDelta, uint rewardsIndex);
-    
-    event ClaimRewards(address indexed owner, uint256 amount);
-
-    struct RewardsState {
-        /// @notice The market's last updated index
-        uint224 index;
-
-        /// @notice The timestamp the index was last updated at
-        uint32 lastUpdatedTimestamp;
-    }
-
-    /// @notice The token to reward
+    /// @notice The token distributed among users.
     ERC20 public immutable rewardToken;
 
-    /// @notice the rewards contract for managing streams
-    IFlywheelRewards public flywheelRewards;
+    /// @notice The fixed point factor of Flywheel.
+    uint224 internal constant ONE = 1e18;
 
-    /// @notice optional booster module for calculating virtual balances on markets
-    IFlywheelBooster public immutable flywheelBooster;
-
-    /// @notice the fixed point factor of flywheel
-    uint224 public constant ONE = 1e18;
-
-    /// @notice The market index and last updated per market
-    mapping(ERC20 => RewardsState) public marketState;
-
-    /// @notice user index per market
-    mapping(ERC20 => mapping(address => uint224)) public userIndex;
-
-    /// @notice The accrued but not yet transferred rewards for each user
-    mapping(address => uint256) public rewardsAccrued;
-
-    /// @dev immutable flag for short-circuiting boosting logic
-    bool internal immutable applyBoosting;
-
+    /// @notice Creates a new FlywheelCore contract.
+    /// @param _rewardToken The address of the token distributed among token holders.
+    /// @param _flywheelRewards The address of the FlywheelRewards module contract.
+    /// @param _flywheelBooster The address of the FlywheelBooster module contract.
+    /// @param _owner The address of the contract owner.
+    /// @param _authority The address of the Authority contract.
     constructor(
-        ERC20 _rewardToken, 
-        IFlywheelRewards _flywheelRewards, 
+        ERC20 _rewardToken,
+        IFlywheelRewards _flywheelRewards,
         IFlywheelBooster _flywheelBooster,
         address _owner,
         Authority _authority
     ) Auth(_owner, _authority) {
+        // Set contract addresses.
         rewardToken = _rewardToken;
         flywheelRewards = _flywheelRewards;
         flywheelBooster = _flywheelBooster;
 
+        // Set the applyBoosting variable to true if a booster contract is set.
         applyBoosting = address(_flywheelBooster) != address(0);
     }
 
-    /// @notice initialize a new market
-    function addMarketForRewards(ERC20 market) external requiresAuth {
-        require(marketState[market].index == 0, "market");
-        marketState[market] = RewardsState({
-            index: ONE,
-            lastUpdatedTimestamp: uint32(block.timestamp)
-        });
+    /*///////////////////////////////////////////////////////////////
+                          BOOSTER CONFIGURATION
+    //////////////////////////////////////////////////////////////*/
 
-        emit AddMarket(address(market));
-    }
+    /// @notice Immutable indicating whether the contract has boosting enabled.
+    bool public immutable applyBoosting;
 
-    /// @notice swap out the flywheel rewards contract
+    /// @notice The FlyWheelBooster contract address.
+    IFlywheelBooster public immutable flywheelBooster;
+
+    /*///////////////////////////////////////////////////////////////
+                          REWARD CONFIGURATION
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice The FlyWheelRewards contract address.
+    IFlywheelRewards public flywheelRewards;
+
+    /// @notice Emitted when the booster module is changed.
+    /// @param user The authorized user who triggered the change.
+    /// @param oldFlywheelRewards The old FlywheelRewards contract address.
+    /// @param newFlywheelRewards The new FlywheelRewards contract address.
+    event FlywheelRewardsUpdated(
+        address indexed user,
+        IFlywheelRewards indexed oldFlywheelRewards,
+        IFlywheelRewards indexed newFlywheelRewards
+    );
+
+    /// @notice Set a new FlywheelRewards contract.
     function setFlywheelRewards(IFlywheelRewards newFlywheelRewards) external requiresAuth {
-        address oldFlywheelRewards = address(flywheelRewards);
+        // Store the old contract address.
+        IFlywheelRewards oldFlywheelRewards = flywheelRewards;
 
+        // Update the FlywheelRewards contract address.
         flywheelRewards = newFlywheelRewards;
 
-        emit FlywheelRewardsUpdate(oldFlywheelRewards, address(newFlywheelRewards));
+        // Emit the event.
+        emit FlywheelRewardsUpdated(msg.sender, oldFlywheelRewards, newFlywheelRewards);
     }
 
-    /// @notice accrue rewards for a single user on a market
+    /*///////////////////////////////////////////////////////////////
+                          MARKET CONFIGURATION
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Packed struct used to store a market's index and the timestamp of its last update.
+    struct RewardsState {
+        /// @notice The market's last updated index.
+        /// @dev This value is scaled by 1e18.
+        uint224 index;
+        /// @notice The timestamp the index was last updated at.
+        uint32 lastUpdatedTimestamp;
+    }
+
+    /// @notice The market index and last updated per market
+    mapping(ERC20 => RewardsState) public marketState;
+
+    /// @notice Emitted when a new market is added.
+    /// @param user The authorized user who triggered the change.
+    /// @param newMarket The new market address.
+    event MarketAdded(address indexed user, ERC20 indexed newMarket);
+
+    /// @notice Add a new ERC20 Market contract to distribute rewards to.
+    /// @param market The address of the market to add.
+    function addMarket(ERC20 market) external requiresAuth {
+        // Ensure the market hasn't already been added.
+        require(marketState[market].index == 0, "MARKET_ALREADY_ADDED");
+
+        // Add the market to the marketState map.
+        marketState[market] = RewardsState({index: 1e18, lastUpdatedTimestamp: uint32(block.timestamp)});
+
+        // Emit the event.
+        emit MarketAdded(msg.sender, market);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                               REWARD LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice The accrued rewards per user, not including claimed or transfered rewards.
+    mapping(address => uint256) public rewardsAccrued;
+
+    /// @notice User indexes per market.
+    mapping(ERC20 => mapping(address => uint224)) public userIndices;
+
+    /// @notice Emitted after a Rewards Accrual.
+    /// @param market The address of the market whose state was updated.
+    /// @param user The address of the user whose accrued rewards.
+    /// @param rewardsDelta The delta between the user's previous index and the current market index.
+    /// @param rewardsIndex Current market index.
+    event RewardsAccrued(ERC20 indexed market, address indexed user, uint256 rewardsDelta, uint256 rewardsIndex);
+
+    /// @notice Accrue rewards for a single user on a market.
+    /// @param market The address of the market to accrue rewards for.
+    /// @param user The address of the user to accrue rewards for.
+    /// @return Accumulated rewards.
     function accrue(ERC20 market, address user) public returns (uint256) {
+        // Cache the global market state.
         RewardsState memory state = marketState[market];
 
+        // If the market has not been added, return 0.
         if (state.index == 0) return 0;
 
+        // Accrue market rewards.
         state = accrueMarket(market, state);
+
+        // Calculate, store, and return the user's accrued rewards.
         return accrueUser(market, user, state);
     }
 
-    /// @notice accrue rewards for two users on a market
-    function accrue(ERC20 market, address user, address secondUser) public returns (uint256, uint256) {
+    /// @notice Accrue rewards for two users on a market.
+    /// @param market The address of the market to accrue rewards for.
+    /// @param user1 The address of the first user to accrue rewards for.
+    /// @param user2 The address of the second user to accrue rewards for.
+    /// @return Accumulated rewards for both users.
+    function accrue(
+        ERC20 market,
+        address user1,
+        address user2
+    ) public returns (uint256, uint256) {
+        // Cache the global market state.
         RewardsState memory state = marketState[market];
 
+        // If the market has not been added, return 0 for both users.
         if (state.index == 0) return (0, 0);
 
+        // Accrue market rewards.
         state = accrueMarket(market, state);
-        return (accrueUser(market, user, state), accrueUser(market, secondUser, state));
+
+        // Calculate, store, and return the users' accrued rewards.
+        return (accrueUser(market, user1, state), accrueUser(market, user2, state));
     }
 
-    /// @notice claim rewards for a given owner
-    function claimRewards(address owner) external {
-        uint256 accrued = rewardsAccrued[owner];
+    /// @notice Accumulate global rewards on a given market.
+    /// @param market The address of the market to accumulate rewards for.
+    /// @param state The state of the market at the time of the reward accumulation.
+    /// @return newState The new market state.
+    function accrueMarket(ERC20 market, RewardsState memory state) internal returns (RewardsState memory) {
+        // Calculate the accrued rewards using the FlywheelRewards module.
+        uint256 accrued = flywheelRewards.getAccruedRewards(market, state.lastUpdatedTimestamp);
 
-        if (accrued != 0) {
-            rewardsAccrued[owner] = 0;
+        // If rewards have not accrued, return the current state.
+        if (accrued == 0) return state;
 
-            rewardToken.safeTransfer(owner, accrued); 
+        // If boosting is enabled, use it to calculate the Rewards Index denominator.
+        // Otherwise just use the market's total supply.
+        uint256 totalSupply = applyBoosting ? flywheelBooster.boostedTotalSupply(market) : market.totalSupply();
 
-            emit ClaimRewards(owner, accrued);
-        }
+        // Accumulate token rewardds onto the index.
+        RewardsState memory newState = RewardsState({
+            index: state.index + (uint224((accrued * ONE) / totalSupply)),
+            lastUpdatedTimestamp: uint32(block.timestamp)
+        });
+
+        // Update the market state.
+        marketState[market] = newState;
+
+        // Return the new state.
+        return newState;
     }
 
-    /// @notice accumulate global rewards on a market
-    function accrueMarket(ERC20 market, RewardsState memory state) private returns(RewardsState memory rewardsState) {
-        // calculate accrued rewards through module
-        uint256 marketRewardsAccrued = flywheelRewards.getAccruedRewards(market, state.lastUpdatedTimestamp);
+    /// @notice Accumulate rewards on a specific market for a given user.
+    /// @param market The address of the market to accumulate rewards for.
+    /// @param user The address of the user to accumulate rewards for.
+    /// @param state The state of the market at the time of the reward accumulation.
+    /// @return Accumulated rewards for the user.
+    function accrueUser(
+        ERC20 market,
+        address user,
+        RewardsState memory state
+    ) internal returns (uint256) {
+        // Load indices.
+        uint224 marketIndex = state.index;
+        uint224 userIndex = userIndices[market][user];
 
-        rewardsState = state;
-        if (marketRewardsAccrued > 0) {
-            // use the booster or token supply to calculate reward index denominator
-            uint256 supplyTokens = applyBoosting ? flywheelBooster.boostedTotalSupply(market): market.totalSupply();
+        // Sync user index to market index.
+        userIndices[market][user] = marketIndex;
 
-            // accumulate rewards per token onto the index, multiplied by fixed-point factor
-            rewardsState = RewardsState({
-                index: state.index + uint224(marketRewardsAccrued * ONE / supplyTokens),
-                lastUpdatedTimestamp: uint32(block.timestamp)
-            });
-            marketState[market] = rewardsState;
-        }
+        // If a user has yet to accrue rewards, grant them interest from the market beginning (if they have a balance).
+        userIndex == 0 ? ONE : userIndex;
+
+        // Calculate the delta between the market and user indices.
+        uint224 indexDelta = marketIndex - userIndex;
+
+        // If boosting is enabled, use it to calculate the reward balance multiplier.
+        // Otherwise just use the user's token balance.
+        uint256 userBalance = applyBoosting ? flywheelBooster.boostedBalanceOf(market, user) : market.balanceOf(user);
+
+        // Accumulate rewards by multiplying user tokens by the delta and adding it to the accrued rewards.
+        uint256 userDelta = (userBalance * indexDelta) / ONE;
+        uint256 accrued = rewardsAccrued[user] + userDelta;
+
+        // Store the new accrued reward amount.
+        rewardsAccrued[user] = accrued;
+
+        // Emit the event.
+        emit RewardsAccrued(market, user, userDelta, marketIndex);
+
+        // Return the new amount.
+        return accrued;
     }
 
-    /// @notice accumulate rewards on a market for a specific user
-    function accrueUser(ERC20 market, address user, RewardsState memory state) private returns (uint256) {
-        // load indices
-        uint224 supplyIndex = state.index;
-        uint224 supplierIndex = userIndex[market][user];
+    /*///////////////////////////////////////////////////////////////
+                               REWARD LOGIC
+    //////////////////////////////////////////////////////////////*/
 
-        // sync user index to global
-        userIndex[market][user] = supplyIndex;
+    /// @notice Emitted after a user claims their rewards.
+    /// @param user The address of the user claimed their rewards.
+    /// @param amount The amount claimed.
+    event RewardsClaimed(address indexed user, uint256 amount);
 
-        // if user hasn't yet accrued rewards, grant them interest from the market beginning if they have a balance
-        // zero balances will have no effect other than syncing to global index
-        if (supplierIndex == 0) {
-            supplierIndex = ONE;
-        }
+    /// @notice Claim
+    function claimRewards(address user) external {
+        // Retrieve the user's accrued rewards.
+        uint256 accrued = rewardsAccrued[user];
 
-        uint224 deltaIndex = supplyIndex - supplierIndex;
-        // use the booster or token balance to calculate reward balance multiplier
-        uint256 supplierTokens = applyBoosting ? flywheelBooster.boostedBalanceOf(market, user) : market.balanceOf(user);
+        // If no rewards have been accrued, return.
+        if (accrued == 0) return;
 
-        // accumulate rewards by multiplying user tokens by rewardsPerToken index and adding on unclaimed
-        uint256 supplierDelta = supplierTokens * deltaIndex / ONE;
-        uint256 supplierAccrued = rewardsAccrued[user] + supplierDelta;
-        
-        rewardsAccrued[user] = supplierAccrued;
+        // Reset the user's accrued rewards.
+        delete rewardsAccrued[user];
 
-        emit AccrueRewards(market, user, supplierDelta, supplyIndex);
+        // Transfer the accrued rewards to the user.
+        rewardToken.safeTransferFrom(address(flywheelRewards), user, accrued);
 
-        return supplierAccrued;
+        // Emit the event.
+        emit RewardsClaimed(user, accrued);
     }
 }
